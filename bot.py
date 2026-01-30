b50f197421b7949330f079d082ed2fd787b7821d
Natali <141375010+NataTusia@users.noreply.github.com>
2026-01-30 02:44:54 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 # Налаштування AI (Gemini)
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-2.0-flash')
+model = genai.GenerativeModel('gemini-flash-latest')
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
